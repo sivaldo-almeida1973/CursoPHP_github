6b323b5a8563cc3e5569d2a6aab7560d7d50c9f5
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-09 11:08:17 -0300

    criado pagina op_aritimeticos.php

---
+++ ex0008/op_aritimeticos.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Operadores aritiméticos</title>
+</head>
+<body>
+    <?php
+       // atenção para concatenação
+      //  $num = "2" + "2";
+      //  echo"$num", ' Nesse caso soma';
+   
+      //  echo "<br>";
+      //  $num = "2" . "2";
+      //  echo"$num", ' Nesse caso concatena ';
+
+
+      //  // tera um erro
+      // //  $nome = 'sivaldo' +'vieira';
+      // //  echo "$nome";
+      //  echo '<br>';
+      //  // jeito certo
+      //  $nome = 'sivaldo' .' vieira';
+      //  echo "$nome";   
+      //  echo "<br>";
+      //  var_dump($nome)    
+
+      
+    
+    ?>
+  
+</body>
+</html>
