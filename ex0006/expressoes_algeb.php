7089d6fd2fb7cdb4cc53831272d30c1fd8261695
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-09 09:00:20 -0300

    criado pagina expressoes_alg.php

---
+++ ex0006/expressoes_algeb.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Expressoes algebricas</title>
+</head>
+<body>
+  <?php 
+  //ordem de precedencia **, potencia, * mult ,/divis ,%modulo
+  // + adição , -subtração
+
+      $res = 5+2/2;
+      echo "O resultado é $res";
+      
+      echo "<br>";
+      $calc = 50/2+3**2;
+      echo "O calculo é $calc";
+      
+
+      echo "<br>";
+      $calc = 50/(2+3)**2;
+      echo "O calculo é $calc";
+      
+
+
+  
+  
+  ?>
+  
+</body>
+</html>
