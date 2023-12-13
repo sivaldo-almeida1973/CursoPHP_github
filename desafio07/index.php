7e91214d7f5aa219f69a338f15e72615a546fe9a
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-13 19:43:52 -0300

    criado  e modificado desafio07

--- desafio07/index.php
+++ desafio07/index.php
@@ -3,37 +3,47 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Exercicio PHP</title>
+  <title>Desafio PHP</title>
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
     <?php 
     //Capiturando os dados do Formulario Retroalimentado
-    $valor1 = $_GET['v1'] ?? 0;
-    $valor2 = $_GET['v2'] ?? 1;
+    $minimo = 1_421.00;
+    $salario = $_GET['sal'] ?? $minimo;
+
    
     ?>
   <main>
-    <h1>Somador de Valores</h1>
+    <h1>Informe seu salário</h1>
+
     <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
-      <label for="v1">Valor 1</label>
-      <input type="number" name="v1" id="v1" value="<?=$valor1?>">
-      <label for="v2">Valor 2</label>
-      <input type="number" name="v2" id="v2" value="<?=$valor2?>">
-      <input type="submit" value="Somar">
+        <label for="sal">Salário (R$)</label>
+        <input type="number" name="sal" id="sal" value="<?=$salario?>" step="0.01">
+        <p>Considerando o salário mínimo de<strong>  R$<?=number_format($minimo, 2, ",", ".")?></strong></p>
+        <input type="submit" value="Calcular">
+  
+  
     </form>
+   
   </main>
 
   <section id="resultado">
+    
 
-    <h2>Resultado da Soma</h2>
+    <h2>Resultado Final</h2>
 
     <?php 
-       $soma = $valor1 / $valor2;
-       echo "<p>A Divisão entre os valores $valor1 e $valor2 é <strong>= $soma</strong></p>";
+    //faz o calulo de quantos salarios minimos recebe 
+       $tot = intdiv($salario, $minimo);
+       $dif = $salario % $minimo;
+
+       echo"<p>Você ganha R\$".number_format($salario, 2, ",",".").' Reais',"($tot salarios minimos + R\$ ".number_format($dif, 2, ",", ".") ." reais).</p>";
       
     ?>
+
+   
   </section>
 
   
